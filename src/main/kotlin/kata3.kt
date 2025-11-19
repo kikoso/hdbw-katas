b7c0b8497fa2d866396188516207b25565fc5984
Enrique López Mañas <eenriquelopez@gmail.com>
2025-11-19 12:02:45 +0100

    Sample main function for Kata 3

--- src/main/kotlin/kata3.kt
+++ src/main/kotlin/kata3.kt
@@ -8,3 +8,13 @@ fun eval(expr: Expr): Int =
 interface Expr
 class Num(val value: Int) : Expr
 class Sum(val left: Expr, val right: Expr) : Expr
+
+fun main() {
+    println(
+        eval(
+            Sum(
+                Num(3), Num(4)
+            )
+        )
+    )
+}
